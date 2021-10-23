7de83055c84063cd45fa908c101109cbe9bbcc3b
Cjabelvinod <92924773+Cjabelvinod@users.noreply.github.com>
2021-10-23 10:41:47 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -30,8 +30,8 @@ Bot Owner Only
 ABOUT = """
 ➥ Name :  Auto Filter V3
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [JUSUS](
+@oppo123455)
 
 ➥ Language : Python3
 
