bb07fb1dd0128f8cbf53b1b9b0ff1308054fc57d
Cjabelvinod <92924773+Cjabelvinod@users.noreply.github.com>
2021-11-09 16:52:52 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -84,7 +84,7 @@ async def start(bot, message):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('⚠Join for movies⚠', url=f'https://t.me/malayalam_films_channel')
+                        InlineKeyboardButton('⚠Join for movies⚠', url=f'https://t.me/malayalam_film_channel')
                     ]
                     ]
                 await bot.send_cached_media(
